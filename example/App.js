83ce18a51720298da251a35fe921d9d2cf6f0c07
Talha Salt <talhaslt@gmail.com>
2021-09-23 13:10:57 +0300

    (ref): refactor example app

--- example/App.js
+++ example/App.js
@@ -6,109 +6,81 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {
   SafeAreaView,
-  StyleSheet,
-  ScrollView,
   View,
   Text,
   StatusBar,
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+
+import {CustomSegmentedControl} from './build/dist/'
 
 const App: () => React$Node = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+        <View style={{ padding: 15 }}>
+          <CustomSegmentedControl
+              segments={["Segment1", "Segment2", "Segment3"]}
+              selected={0}
+              height={55}
+              borderWidth={1}
+              borderColor={"#9d9d9d"}
+              borderRadius={15}
+              selectedSegmentColor={"#0082ff"}
+              unselectedSegmentsColor={"#e7e5e5"}
+              selectedTextColor={"#fff"}
+              unselectedTextColor={"#000"}
+              textFontSize={20}
+              onChangeSegment={(index) => setSelectedIndex(index)}
+          />
+          <View style={{ marginTop: 15 }}>
+            {selectedIndex == 0 ? (
+                <View
+                    style={{
+                      width: "100%",
+                      backgroundColor: "red",
+                      height: 100,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                >
+                  <Text>{selectedIndex}</Text>
+                </View>
+            ) : selectedIndex == 1 ? (
+                <View
+                    style={{
+                      width: "100%",
+                      backgroundColor: "blue",
+                      height: 100,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                >
+                  <Text>{selectedIndex}</Text>
+                </View>
+            ) : selectedIndex == 2 ? (
+                <View
+                    style={{
+                      width: "100%",
+                      backgroundColor: "green",
+                      height: 100,
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                >
+                  <Text>{selectedIndex}</Text>
+                </View>
+            ) : null}
           </View>
-        </ScrollView>
+        </View>
       </SafeAreaView>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
